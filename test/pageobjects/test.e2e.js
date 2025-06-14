cb1c8b9513ab03a3d8d6f2f7ce969134695f7915
Phani Dasari <phanidasari6351@gmail.com>
2025-06-14 18:08:32 +0530

    Updated test.e2e.js with Facebook login test

---
+++ test/pageobjects/test.e2e.js
@@ -0,0 +1,26 @@
+describe('Facebook Login Page Test', () => {
+    it('should open Facebook and try to login', async () => {
+        await browser.url('https://www.facebook.com/');
+
+        // Accept cookies if popup appears
+        const acceptCookies = await $('button[data-cookiebanner="accept_button"]');
+        if (await acceptCookies.isDisplayed()) {
+            await acceptCookies.click();
+        }
+
+        // Enter email
+        const emailInput = await $('#email');
+        await emailInput.setValue('testuser@example.com');
+
+        // Enter password
+        const passwordInput = await $('#pass');
+        await passwordInput.setValue('TestPassword123');
+
+        // Click login button
+        const loginButton = await $('button[name="login"]');
+        await loginButton.click();
+
+        // Wait for some time to see result
+        await browser.pause(3000);
+
+        //
