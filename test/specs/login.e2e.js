3a1a2f31c460f234bfe0e0d86867f7fdd981f0d0
Phani Dasari <phanidasari6351@gmail.com>
2025-06-22 16:58:21 +0530

    payment gateway

--- test/specs/login.e2e.js
+++ test/specs/login.e2e.js
@@ -1,27 +1,38 @@
-describe('Add to Cart Functionality - AutomationExercise', () => {
-  it('should add a product to the cart successfully', async () => {
+describe('Payment Gateway Test - AutomationExercise', () => {
+  it('should complete the payment with valid card', async () => {
+    // Open website
     await browser.url('https://automationexercise.com');
 
-    await browser.pause(2000);
-    await browser.execute(() => window.scrollBy(0, 600));
+    // You must already be logged in and have item in cart (you can reuse add-to-cart + login script)
 
-    const firstProduct = await $('.product-overlay');
-    await firstProduct.waitForDisplayed({ timeout: 5000 });
-    await firstProduct.moveTo
-
-    const addToCartBtn = await $('(//a[contains(text(),"Add to cart")])[1]');
-    await addToCartBtn.click();
-
-    const modal = await $('#cartModal');
-    await modal.waitForDisplayed({ timeout: 5000 });
-
-    const continueBtn = await $('button[data-dismiss="modal"]');
-    await continueBtn.click();
-
+    // Go to cart and proceed to checkout
     const cartBtn = await $('a[href="/view_cart"]');
     await cartBtn.click();
 
-    const cartProduct = await $('.cart_description');
-    await expect(cartProduct).toBeDisplayed();
+    const checkoutBtn = await $('a.btn.btn-default.check_out');
+    await checkoutBtn.click();
+
+    // Fill delivery information and continue (skipped here for simplicity)
+
+    // Now you are on payment form
+    const nameOnCard = await $('input[name="name_on_card"]');
+    const cardNumber = await $('input[name="card_number"]');
+    const cvc = await $('input[name="cvc"]');
+    const expiryMonth = await $('input[name="expiry_month"]');
+    const expiryYear = await $('input[name="expiry_year"]');
+
+    // Fill payment details
+    await nameOnCard.setValue('Phaneendra Kumar');
+    await cardNumber.setValue('4111111111111111'); // dummy Visa card
+    await cvc.setValue('123');
+    await expiryMonth.setValue('12');
+
+    // Click Pay button
+    const payBtn = await $('button#submit');
+    await payBtn.click();
+
+    // Assert payment success message
+    const successMsg = await $('p.alert-success');
+    await expect(successMsg).toHaveTextContaining('Your order has been placed successfully!');
   });
 });
